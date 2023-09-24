d089fca3a5def8f24af581fd74c996441a791133
Hingaz <hhinga04@gmail.com>
2023-09-24 10:49:19 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = function() {
+  
+  return 2000 / 100;
+};
+
+
+
+const square = (number) => {
+  
+  return number * number;
+};
+
+
+const add = (num1, num2) => {
+  
+  return num1 + num2;
+};
